b1bf51d7fd0f77270189c81216e94350dfbd890a
hsmgebaly <hsmgebaly@gmail.com>
2023-04-09 20:05:48 -0700

    0x09

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -3,18 +3,16 @@
  * _strlen - returns the length of a string
  * @s: string
  * Return: length
-*/
-int_strlen(char *s)
+ */
+int _strlen(char *s)
 {
 	int longi = 0;
-
 
 	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
-
 
 	return (longi);
 }
