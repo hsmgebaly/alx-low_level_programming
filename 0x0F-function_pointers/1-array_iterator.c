8e4054d33f0fc06835f83c885962425bf0a56e32
hsmgebaly <hsmgebaly@gmail.com>
2023-04-19 19:54:00 -0700

    pintersfunc

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - prints each array on a new
+ * @array: array
+ * @size: how many elemnts to print
+ * @action: pointer to print in regualr or hex
+ * Return: void
+ **/
+void array_iterator(int *array, size_t size, void (*acction)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,9 @@
+C - Function pointers
+
+0. What's my name
+1. If you spend too much time thinking about a thing, you'll never get it done
+2. To hell with circumstances; I create opportunities
+Write a function that searches for an integer.
+3. A goal is not always meant to be reached, it often serves simply as something to aim at
+Write a program that performs simple operation.
+
